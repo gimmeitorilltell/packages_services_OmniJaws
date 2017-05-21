67b8e52eaf273f8da765efe0349c29c69ee472c9
Alex Cruz <du.alexcruz@gmail.com>
2017-05-21 20:26:02 +0200

    Change OWM API key - READ COMMIT MESSAGE!
    DO NOT MERGE in your own ROM! This isn't because we want to be assholes
    about it but because OWM allows only 60 free calls per minute and the
    more people that use this API key, the more calls are send their way. We
    end up losing access and if you use this then you will lose access.
    
    Nobody wins if you decide to use our keys.
    
    Create your own, takes 30 seconds
    https://openweathermap.org/appid#get
    
    Change-Id: I2abd8e78c7107a9a7974600ec444140ccc5f0965

--- src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -38,7 +38,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private static final int FORECAST_DAYS = 5;
     private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
     private static final String SELECTION_ID = "id=%s";
-    private static final String API_KEY = "9f65b19b7a6648346dda93c6973a682c";
+    private static final String API_KEY = "2839733220575eead0e88b53f7ee2b77";
 
     private static final String URL_LOCATION =
             "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid=%s";
@@ -194,7 +194,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
         }
         return (float) value;
     }
-    
+
     private static final HashMap<String, String> LANGUAGE_CODE_MAPPING = new HashMap<String, String>();
     static {
         LANGUAGE_CODE_MAPPING.put("bg-", "bg");
